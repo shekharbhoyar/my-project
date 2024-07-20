07b34b4b0db15389c77c5aad6dd45d13bb272f02
chandrashekhar <shekharbhoyar10@gmail.com>
2024-07-20 10:06:18 +0530

    my first file

---
+++ first.js
@@ -0,0 +1,17 @@
+// let a="hello world"
+// console.log(a)
+// let a="harry"
+// let b=28
+// let c=BigInt("384")
+// let d=Symbol("Iam a nice symbol")
+// let e=null
+// let f=true
+// let g=undefined
+// console.log(a,b,c,d,e,f,g,)
+// console.log(
+let a = 4
+let b = 5
+console.log("++a=", ++a)
+console.log("a ", a)
+console.log("a++=", a++)
+console.log("a ", a)
